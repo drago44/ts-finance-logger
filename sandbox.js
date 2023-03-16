e5ce0949d9dfcc40941a5357f0af631493161dd2
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-16 19:16:08 +0200

    type basics

--- sandbox.js
+++ sandbox.js
@@ -1,6 +1,13 @@
-var character = 'mario';
+var character = 'mar';
+var singIn = false;
+var list = [1, 2, 3];
+var circ = function (diameter) {
+    return diameter * Math.PI;
+};
+character = 'alex';
+singIn = true;
+list.push(4);
 console.log(character);
-var inputs = document.querySelectorAll('input');
-inputs.forEach(function (input) {
-    console.log(input);
-});
+console.log(list);
+console.log(singIn);
+console.log(circ(444));
--- sandbox.ts
+++ sandbox.ts
@@ -1,9 +1,16 @@
-const character = 'mario';
+let character: string = 'mar';
+let singIn: boolean = false;
+let list: number[] = [1, 2, 3];
+
+const circ = (diameter: number) => {
+  return diameter * Math.PI;
+};
+
+character = 'alex';
+singIn = true;
+list.push(4);
 
 console.log(character);
-
-const inputs = document.querySelectorAll('input');
-
-inputs.forEach(input => {
-  console.log(input);
-});
+console.log(list);
+console.log(singIn);
+console.log(circ(444));

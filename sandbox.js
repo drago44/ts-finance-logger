cff2597c22d44c7bd8ab7b65bb6c782431fabb07
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-16 19:27:32 +0200

    objects & arrays

--- sandbox.js
+++ sandbox.js
@@ -1,13 +1,28 @@
-var character = 'mar';
-var singIn = false;
-var list = [1, 2, 3];
-var circ = function (diameter) {
-    return diameter * Math.PI;
+// arrays
+var names = ['luigi', 'mario', 'yoshi'];
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+var numbers = [10, 20, 12, 15];
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+var mixed = ['ken', 4, 'chun-li', 8, 9];
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+// objects
+var ninja = {
+    name: 'mario',
+    belt: 'black',
+    age: 30
 };
-character = 'alex';
-singIn = true;
-list.push(4);
-console.log(character);
-console.log(list);
-console.log(singIn);
-console.log(circ(444));
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
+ninja = {
+    name: 'yoshi',
+    belt: 'orange',
+    age: 40
+};
--- sandbox.ts
+++ sandbox.ts
@@ -1,16 +1,41 @@
-let character: string = 'mar';
-let singIn: boolean = false;
-let list: number[] = [1, 2, 3];
+// arrays
+let names: string[] = ['luigi', 'mario', 'yoshi'];
 
-const circ = (diameter: number) => {
-  return diameter * Math.PI;
+names.push('toad');
+// names.push(3);
+// names[1] = 3;
+
+let numbers: number[] = [10, 20, 12, 15];
+
+numbers.push(25);
+// numbers.push('shaun');
+// numbers[0] = 'shaun';
+
+let mixed: (string | number)[] = ['ken', 4, 'chun-li', 8, 9];
+
+mixed.push('ryu');
+mixed.push(10);
+mixed[0] = 3;
+
+// objects
+let ninja: {
+  name: string;
+  belt: string;
+  age: number;
+} = {
+  name: 'mario',
+  belt: 'black',
+  age: 30,
 };
 
-character = 'alex';
-singIn = true;
-list.push(4);
+ninja.age = 40;
+ninja.name = 'ryu';
+// ninja.age = '30';
+// ninja.skills = ['fighting', 'sneaking']
 
-console.log(character);
-console.log(list);
-console.log(singIn);
-console.log(circ(444));
+ninja = {
+  name: 'yoshi',
+  belt: 'orange',
+  age: 40,
+  // skills: ['running'],
+};

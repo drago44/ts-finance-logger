fc24ddcd9d03506591d0cc48462a64b958b97548
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-17 15:21:41 +0200

    Function Signatures

--- public/sandbox.js
+++ public/sandbox.js
@@ -1,7 +1,22 @@
 "use strict";
-var greet = function (user) {
-    console.log("".concat(user.name, " say hello"));
+// let greet: Function;
+// example 1
+var greet;
+greet = function (name, greeting) {
+    console.log("".concat(name, " says ").concat(greeting));
 };
-var greetAgain = function (user) {
-    console.log("This ".concat(user.uid));
+// example 2
+var calc;
+calc = function (numOne, numTwo, action) {
+    if (action === 'add') {
+        return numOne + numTwo;
+    }
+    else {
+        return numOne - numTwo;
+    }
+};
+// example 3
+var logDetails;
+logDetails = function (ninja) {
+    console.log("".concat(ninja.name, " is ").concat(ninja.age, " years old"));
 };

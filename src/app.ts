72997e08d846526db2472ebab85594273f6f2b14
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-17 16:14:06 +0200

    Interfaces

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,32 @@
-import { Invoice } from './classes/Invoice.js';
+// interfaces
+export interface IsPerson {
+  name: string;
+  age?: number;
+  speak(a: string): void;
+  spend(a: number): number;
+}
 
-const invOne = new Invoice('mario', 'work on the mario website', 250);
-const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+const me: IsPerson = {
+  name: 'shaun',
+  //age: 30,
+  speak(text: string): void {
+    console.log(text);
+  },
+  spend(amount: number): number {
+    console.log('I spent ', amount);
+    return amount;
+  },
+};
 
-// invOne.client = 'yoshi';
-// invOne.amount = 50;
+console.log(me);
+me.speak('hello, world');
 
-let invoices: Invoice[] = [];
-invoices.push(invOne);
-invoices.push(invTwo);
+const greetPerson = (person: IsPerson): void => {
+  console.log('hello ', person.name);
+};
 
-invoices.forEach((inv) => {
-  console.log(inv.client, /*inv.details,*/ inv.amount, inv.format());
-});
+greetPerson(me);
+//greetPerson({name: 'shaun'});
 
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);

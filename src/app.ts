b8a7a4aacce46371eff7b06a4289edb74298dfa0
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-17 17:53:56 +0200

    Rendering an HTML Template and use localStorage

--- src/app.ts
+++ src/app.ts
@@ -1,25 +1,39 @@
 import { Invoice } from './classes/Invoice.js';
 import { Payment } from './classes/Payment.js';
+import { ListTemplate } from './classes/ListTemplate.js';
 import { HasFormatter } from './interfaces/HasFormatter.js';
 
-// let docOne: HasFormatter;
-// let docTwo: HasFormatter;
-
-// docOne = new Invoice('yoshi', 'web work', 250);
-// docTwo = new Payment('mario', 'plumbing', 200);
-
-// let docs: HasFormatter[] = [];
-// docs.push(docOne);
-// docs.push(docTwo);
-
 const form = document.querySelector('.new-item-form') as HTMLFormElement;
-console.log(form.children);
 
 // inputs
 const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;
+
+// list template instance
+const ul = document.querySelector('ul')!;
+const list = new ListTemplate(ul);
+
+type OldDoc = [
+  string,
+  { client?: string; recipient?: string; details: string; amount: number },
+];
+const oldDoc: OldDoc[] = JSON.parse(localStorage.getItem('doc') || '[]');
+
+oldDoc.forEach((item) => {
+  const [type, data] = item;
+
+  let doc: HasFormatter;
+
+  if (type === 'invoice') {
+    doc = new Invoice(data.client!, data.details, data.amount);
+  } else {
+    doc = new Payment(data.recipient!, data.details, data.amount);
+  }
+
+  list.render(doc, type, 'end');
+});
 
 form.addEventListener('submit', (e: Event) => {
   e.preventDefault();
@@ -30,5 +44,7 @@ form.addEventListener('submit', (e: Event) => {
   } else {
     doc = new Payment(tofrom.value, details.value, amount.valueAsNumber);
   }
-  console.log(doc);
+
+  list.render(doc, type.value, 'end');
+  localStorage.setItem('doc', JSON.stringify([...oldDoc, [type.value, doc]]));
 });

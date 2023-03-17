7fb6c321d2c8fbf2a43c9a869d2d43f161350d76
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-17 16:03:08 +0200

    Modules

--- src/app.ts
+++ src/app.ts
@@ -1,22 +1,10 @@
-// classes
-class Invoice {
-  // readonly client: string;
-  // private details: string;
-  // public amount: number;
-
-  constructor(
-    readonly client: string,
-    private details: string,
-    public amount: number,
-  ) {}
-
-  format() {
-    return `${this.client} owes £${this.amount} for ${this.details}`;
-  }
-}
+import { Invoice } from './classes/Invoice.js';
 
 const invOne = new Invoice('mario', 'work on the mario website', 250);
 const invTwo = new Invoice('luigi', 'work on the luigi website', 300);
+
+// invOne.client = 'yoshi';
+// invOne.amount = 50;
 
 let invoices: Invoice[] = [];
 invoices.push(invOne);
@@ -30,7 +18,7 @@ const form = document.querySelector('.new-item-form') as HTMLFormElement;
 console.log(form.children);
 
 // inputs
-const type = document.querySelector('#type') as HTMLSelectElement;
+const type = document.querySelector('#type') as HTMLInputElement;
 const tofrom = document.querySelector('#tofrom') as HTMLInputElement;
 const details = document.querySelector('#details') as HTMLInputElement;
 const amount = document.querySelector('#amount') as HTMLInputElement;

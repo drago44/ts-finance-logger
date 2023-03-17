4d25c6187381a851c3e85693aeaad32af282bcca
Alex <oleksander.kucherenko.work@gmail.com>
2023-03-17 18:05:59 +0200

    Generics

--- src/app.ts
+++ src/app.ts
@@ -25,7 +25,6 @@ oldDoc.forEach((item) => {
   const [type, data] = item;
 
   let doc: HasFormatter;
-
   if (type === 'invoice') {
     doc = new Invoice(data.client!, data.details, data.amount);
   } else {
@@ -48,3 +47,46 @@ form.addEventListener('submit', (e: Event) => {
   list.render(doc, type.value, 'end');
   localStorage.setItem('doc', JSON.stringify([...oldDoc, [type.value, doc]]));
 });
+
+// GENERICS
+
+// const addUID = (obj: object) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+// const addUID = <T extends object>(obj: T) => {
+//   let uid = Math.floor(Math.random() * 100);
+//   return {...obj, uid};
+// }
+
+const addUID = <T extends { name: string }>(obj: T) => {
+  let uid = Math.floor(Math.random() * 100);
+  return { ...obj, uid };
+};
+
+let docOne = addUID({ name: 'yoshi', age: 40 });
+//let docTwo = addUID('shaun');
+
+console.log(docOne.name);
+
+// with interfaces
+interface Resource<T> {
+  uid: number;
+  resourceName: string;
+  data: T;
+}
+
+const docThree: Resource<object> = {
+  uid: 1,
+  resourceName: 'person',
+  data: { name: 'shaun' },
+};
+
+const docFour: Resource<string[]> = {
+  uid: 1,
+  resourceName: 'shoppingList',
+  data: ['bread', 'milk'],
+};
+
+console.log(docThree, docFour);
